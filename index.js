8c2b65b95d83c32f381c89947358400b34ad2fdc
Yuval Perets <yuval9330@gmail.com>
2025-08-02 19:14:17 +0300

    Add CORS support

--- index.js
+++ index.js
@@ -12,6 +12,15 @@ ffmpeg.setFfmpegPath(ffmpegPath);
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors = require("cors");
+app.use(cors({
+  origin: ["http://localhost:3000", "https://your-final-domain.com"],
+  methods: ["GET", "POST", "OPTIONS"],
+  allowedHeaders: ["Content-Type"],
+}));
+
+
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
